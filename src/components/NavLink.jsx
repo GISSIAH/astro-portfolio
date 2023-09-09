022634cc59cbe65a0688edf19ac71af943386413
Bryan Malunje <shlattbihness@gmail.com>
2023-09-09 14:01:14 +0200

    migrating to astro 3

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -5,7 +5,7 @@ export default function NavLink({ title, link }) {
   return (
     <a
       href={link}
-      className={`text-xl text-gray-900 font-normal hover:text-blue-400`}
+      className={`text-xl text-gray-900 font-normal hover:text-blue-400 dark:text-gray-200`}
     >
       {title}
     </a>
